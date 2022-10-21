9ed921ab24dbd98e8d3d165b2d7af26e102547ea
Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>
2022-10-21 22:47:48 +0100

    added email validation
    Signed-off-by: Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,28 @@ module.exports.signup_post = async (req, res) => {
     const user = await User.create({ email, password });
     res.status(201).json(user);
   } catch (err) {
-    console.log(err);
-    res.status(400).send("Error: user was not created");
+    const errors = handleErors(err);
+    res.status(400).json({ errors });
   }
+};
+
+// Error Handler
+const handleErors = (err) => {
+  console.log(err.message, err.code);
+  let errorsObj = { email: "", password: "" };
+
+  // Duplicate email error
+  if (err.code === 11000) {
+    errorsObj.email = "That email is already registered... login!";
+    return errorsObj;
+  }
+
+  // Validation Errors
+  if (err.message.includes("user validation failed"))
+    Object.values(err.errors).forEach(
+      (e) => (errorsObj[e.properties.path] = e.properties.message)
+    );
+
+  // console.log(errorsObj);
+  return errorsObj;
 };
--- models/User.js
+++ models/User.js
@@ -1,16 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"),
+  { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, "Please enter an email"],
     unique: true,
     lowercase: true,
+    validate: [isEmail, "Email is not valid!"],
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, "Please enter a password"],
+    minlength: [8, "Password must be at least 8 characters"],
   },
 });
 
