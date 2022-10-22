c3d8edfd515c4c0cef55393a0719629194003e4f
Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>
2022-10-22 12:58:26 +0100

    cookies implementationn
    Signed-off-by: Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express"),
   mongoose = require("mongoose"),
-  authRoutes = require("./routes/authRoutes");
+  authRoutes = require("./routes/authRoutes"),
+  cookieParser = require("cookie-parser");
 
 const app = express();
 
 //   MIDDLEWARE
 app.use(express.static("public")); // to serve static files to the browser
 app.use(express.json());
+app.use(cookieParser());
 
 // VIEW ENGINE
 app.set("view engine", "ejs");
@@ -23,3 +25,22 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/posts", (req, res) => res.render("posts"));
 app.use(authRoutes);
+
+// COOKIES
+app.get("/set-cookies", (req, res) => {
+  // res.setHeader("Set-Cookie", "newUser=true");
+
+  res.cookie("newUser", false);
+  // 1000ms * 60sec * 60min * 24hour (one day to expire)
+  res.cookie("isEmployee", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true, // prevent access by javascript
+  });
+
+  res.send("You just got your cookies");
+});
+
+app.get("/read-cookies", (req, res) => {
+  const cookies = req.cookies;
+  res.json(cookies);
+});
