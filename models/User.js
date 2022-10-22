a6ea26a81b5a3bc686979e18c0dfea031c4ed35b
Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>
2022-10-22 01:34:54 +0100

    hash and salt passwords before saving new user to db
    Signed-off-by: Zouheir-Barhoumi <zouheir.barhoumi@gmail.com>

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose"),
-  { isEmail } = require("validator");
+  { isEmail } = require("validator"),
+  bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -16,6 +17,20 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+// run a function after doc save
+userSchema.post("save", (doc, next) => {
+  console.log("New user has been created and saved", doc);
+  next();
+});
+
+// run a function before doc save (don't use arrow function)
+// use traditional functions to have access to the 'this' keyword locally (user object)
+// we don't get doc because it's still not saved to the DB
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
